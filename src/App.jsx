cfdde70c82543c78a68fc72089900a8c92d75f18
wenhaolu <wenhaolu.kevin@gmail.com>
2024-05-30 11:50:32 -0400

    update static parts

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import PropTypes from "prop-types";
 import { useState } from "react";
 import "./App.css";
+import lottoImage1 from "./assets/lotto-649.png";
+import lottoImage2 from "./assets/lotto-max.png";
 
 function App() {
   const poolNumbers = Array.from({ length: 50 }, (_, i) => i + 1); // create an array contains number 1-50
@@ -18,7 +20,7 @@ function App() {
 
 function LotteryPool({ numbers }) {
   return (
-    <div>
+    <div className="numberSide">
       <div className="numbersContainer">
         {numbers.map((number) => (
           <div className="singleNum" key={number}>
@@ -37,9 +39,24 @@ LotteryPool.propTypes = {
 
 function NumPicking() {
   return (
-    <div>
-      <div>choose type</div>
-      <div>lucky number</div>
+    <div className="sidebar">
+      <div>
+        <div className="chooseContainer">
+          <div className="chooseLottoWrap">
+            <img className="lottoImg-1" src={lottoImage1} alt="lotto-649" />
+            <input type="radio" value="649" name="option" />
+            {/*use 'name' attribute to group the radio inputs together, can only select one */}
+          </div>
+          <div className="chooseLottoWrap">
+            <img className="lottoImg-2" src={lottoImage2} alt="lotto-max" />
+            <input type="radio" value="max" name="option" />
+          </div>
+        </div>
+      </div>
+      <div className="numInputWrap">
+        <label>lucky number</label>
+        <input type="text" />
+      </div>
       <div>output</div>
       <button>feeling lucky</button>
     </div>
