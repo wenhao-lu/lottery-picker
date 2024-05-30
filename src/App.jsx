03391f3dbb437f8d487c8f56fca38ec5108f0012
wenhaolu <wenhaolu.kevin@gmail.com>
2024-05-30 09:38:31 -0400

    update poolNumbers

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,40 @@
+import PropTypes from "prop-types";
 import { useState } from "react";
 import "./App.css";
 
 function App() {
+  const poolNumbers = Array.from({ length: 50 }, (_, i) => i + 1); // create an array contains number 1-50
+  console.log(poolNumbers);
   return (
     <div className="main">
       <h1>Lukcy Lottery Picker</h1>
       <div className="container">
-        <LotteryPool />
+        <LotteryPool numbers={poolNumbers} />
         <NumPicking />
       </div>
     </div>
   );
 }
 
-function LotteryPool() {
+function LotteryPool({ numbers }) {
   return (
     <div>
-      <div>numbers</div>
+      <div className="numbersContainer">
+        {numbers.map((number) => (
+          <div className="singleNum" key={number}>
+            <div className="numberBackground">{number}</div>
+          </div>
+        ))}
+      </div>
       <div>output</div>
     </div>
   );
 }
 
+LotteryPool.propTypes = {
+  numbers: PropTypes.arrayOf(PropTypes.number).isRequired, // expecting an array of numbers
+};
+update;
 function NumPicking() {
   return (
     <div>
